471356c12df25c85f8a9136f56a406006c798be5
rostovtsev-nikita <99068528+rostovtsev-nikita@users.noreply.github.com>
2022-05-08 21:57:10 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,54 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
+
+Train::Train(): countOp(0), first(nullptr) {}
+
+void Train::addCage(bool glim) {
+      Cage *cage = new Cage;
+      cage->glim = glim;
+      cage->next = nullptr;
+      cage->prev = nullptr;
+        if (first == nullptr) {
+            first = cage;
+        } else if (first->prev == nullptr) {
+          first->prev = cage;
+          cage->next = first;
+          first->next = cage;
+          cage->prev = first;
+        } else {
+          first->next->prev = cage;
+          cage->next = first->next;
+          first->next = cage;
+          cage->prev = first;
+      }
+  }
+
+int Train::getLength() {
+      int trainlen = 0;
+      int i = 0;
+      first->glim = true;
+      Cage *temp = first;
+        while (true) {
+            countOp++;
+            trainlen++;
+            temp = temp->prev;
+            if (temp->glim) {
+                temp->glim = false;
+                i = 0;
+                while (i < trainlen) {
+                    temp = temp->next;
+                    countOp++;
+                    i++;
+              }
+                if (!temp->glim) {
+                    return trainlen;
+              }
+              trainlen = 0;
+          }
+      }
+      return i;
+  }
+
+int Train::getOpCount() {
+      return countOp;
+  }
